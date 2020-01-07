01505e88e459ad9711e08b280fd91121443fb6ff
Kayquep <kayquepagiola10@gmail.com>
2020-01-07 13:39:36 -0300

    Creating the files for posts

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,11 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col3 p-5">
+        <div class="col-3 p-5">
             <img src="https://instagram.fudi1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fudi1-1.fna.fbcdn.net&_nc_ohc=Tj6v7qiBykUAX8dRddF&oh=0c30bedf6a64469d9de8b019518126e4&oe=5EAA2DD7" class="rounded-circle">
         </div>
-        <div class="col9 pt-5">
-            <div><h1>{{$user->username}}</h1></div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{$user->username}}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
