e987f7df8f74c992a144055d0ff8f2f2a6594457
Kayquep <kayquepagiola10@gmail.com>
2020-01-07 14:35:07 -0300

    Finished creating the new post controller

--- database/migrations/2020_01_07_160929_create_posts_table.php
+++ database/migrations/2020_01_07_160929_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
-            $table->string('capture');
+            $table->string('caption');
             $table->string('image');
             $table->timestamps();
 
