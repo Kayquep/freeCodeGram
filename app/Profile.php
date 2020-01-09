69cbc2d856ac227217b1e77002cdef4249c90b57
Kayquep <kayquepagiola10@gmail.com>
2020-01-09 14:03:19 -0300

    Fixed the profile image when the user doesn't have one and put a standard one

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/iQQq1stDVjyYcUjwWhUz9Ob1sxJT1yJlsps4wPjR.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
